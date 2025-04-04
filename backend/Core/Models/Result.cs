ea57c013458e6d129ceb87cba00adcdb5d1bdfb8
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-04-04 15:55:20 +0200

    Init

---
+++ backend/Core/Models/Result.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Result
+    {
+        private string[] _errors;
+        public bool IsSuccess => _errors.Length == 0;
+        public string? ErrorMessage => !IsSuccess ? String.Join(' ', _errors) : null;
+
+        protected Result() { }
+
+        public static Result Success()
+        {
+            return new Result();
+        }
+
+        public static Result Failure(params string[] errors)
+        {
+            var result = new Result();
+            result._errors = errors;
+            return result;
+        }
+    }
+
+    public class Result<T> : Result
+    {
+        public T Value { get; private set; }
+
+        public static Result<T> Success(T model)
+        {
+            var result = new Result<T>();
+            result.Value = model;
+            return result;
+        }
+    }
+}
