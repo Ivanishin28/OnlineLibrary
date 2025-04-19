943ca1eb820dd7dfe5c53318fd7365862b902301
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-04-19 19:39:11 +0200

    Add Unit Of Work. Add UserAccountRepository

---
+++ backend/Shared.DL/Implementations/UnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Shared.DL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DL.Implementations
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private DbContext _db;
+
+        public async Task SaveChanges()
+        {
+            await _db.SaveChangesAsync();
+        }
+    }
+}
---
+++ backend/UserContext.DL.SqlServer/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UserContext.Domain.Entities;
+
+namespace UserContext.DL.SqlServer
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<UserAccount> UserAccounts { get; set; }
+        public DbSet<UserProfile> UserProfiles { get; set; }
+    }
+}
