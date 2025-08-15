4ffffb230e7977e66915446f432858775215b64b
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-08-15 16:35:49 +0200

    Install PrimeNg

--- onlib/src/app/app.component.ts
+++ onlib/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { KnobModule } from 'primeng/knob';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KnobModule, ReactiveFormsModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'onlib';
+  public value: number = 0;
 }
