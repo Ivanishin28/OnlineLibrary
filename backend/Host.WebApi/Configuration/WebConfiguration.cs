36384ec031e60ed31a82525482091ad16259b505
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-09-24 17:01:22 +0200

    Register HttpContext

--- backend/Host.WebApi/Configuration/WebConfiguration.cs
+++ backend/Host.WebApi/Configuration/WebConfiguration.cs
@@ -1,14 +1,9 @@
-﻿using Host.WebApi.Web.Providers;
-using Shared.Core.Interfaces;
-
-namespace Host.WebApi.Configuration
+﻿namespace Host.WebApi.Configuration
 {
     public static class WebConfiguration
     {
         public static IServiceCollection RegisterWebServices(this IServiceCollection services)
         {
-            services.AddTransient<IUserContext, HttpUserContext>();
-
             return services;
         }
     }
--- backend/Identity.Application/Configuration/ServicesConfiguration.cs
+++ backend/Identity.Application/Configuration/ServicesConfiguration.cs
@@ -1,6 +1,7 @@
 ﻿using IdentityContext.Application.Concrete;
 using IdentityContext.Application.Interfaces;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Core.Interfaces;
 
 namespace IdentityContext.Application.Configuration
 {
@@ -9,6 +10,7 @@ namespace IdentityContext.Application.Configuration
         public static IServiceCollection RegisterServices(this IServiceCollection services)
         {
             services
+                .AddTransient<IUserContext, HttpUserContext>()
                 .AddTransient<ITokenBuilder, JwtTokenBuilder>();
 
             return services;
