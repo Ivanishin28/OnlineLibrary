38063ae989de1c2c9fea64d1cb7e526994e36651
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-10-17 18:15:06 +0200

    Register composition project

---
+++ backend/Composition.Application/Configuration/UseCasesConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Composition.Application.UseCases.Queries;
+using Composition.Contract.Queries;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ShelfContext.Application.Configuration
+{
+    public static class UseCasesConfiguration
+    {
+        public static IServiceCollection RegisterUseCases(this IServiceCollection services)
+        {
+            services.AddMediatR(config =>
+            {
+                var contractsAssembly = typeof(GetLibraryPageQuery).Assembly;
+                config.RegisterServicesFromAssembly(contractsAssembly);
+
+                var handlersAssembly = typeof(GetLibraryPageQueryHandler).Assembly;
+                config.RegisterServicesFromAssembly(handlersAssembly);
+            });
+
+            return services;
+        }
+    }
+}
--- backend/Composition.Contract/Queries/GetLibraryPageQuery.cs
+++ backend/Composition.Contract/Queries/GetLibraryPageQuery.cs
@@ -2,10 +2,14 @@
 using Composition.Contract.Models;
 using MediatR;
 using Shared.Contracts.Models;
+using System.Text.Json.Serialization;
 
 namespace Composition.Contract.Queries
 {
-    public record GetLibraryPageQuery(Guid UserId, LibraryFilter Filter, Page Page) : IRequest<Pagination<LibraryShelvedBook>>
+    public record GetLibraryPageQuery(
+        [property: JsonPropertyName("user_id")] Guid UserId,
+        [property: JsonPropertyName("filter")] LibraryFilter Filter,
+        [property: JsonPropertyName("page")] Page Page) : IRequest<Pagination<LibraryShelvedBook>>
     {
     }
 }
