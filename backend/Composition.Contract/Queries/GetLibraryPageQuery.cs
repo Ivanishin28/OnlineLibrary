05333f43a7f95bf7fd3184cc5243e6e0522e9e7a
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-10-17 17:40:06 +0200

    Add Composition.Contracts

---
+++ backend/Composition.Contract/Queries/GetLibraryPageQuery.cs
@@ -0,0 +1,11 @@
+﻿using Composition.Contract.Dtos;
+using Composition.Contract.Models;
+using MediatR;
+using Shared.Contracts.Models;
+
+namespace Composition.Contract.Queries
+{
+    public record GetLibraryPageQuery(Guid UserId, LibraryFilter Filter, Page Page) : IRequest<Pagination<LibraryShelvedBook>>
+    {
+    }
+}
