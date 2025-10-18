ce9758f50bc6d1ff6adce7ca52a2b34c0bf4b1f0
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-10-18 13:56:17 +0200

    Create avatar part in navbar

--- onlib/src/app/view/components/header/navbar/navbar.component.ts
+++ onlib/src/app/view/components/header/navbar/navbar.component.ts
@@ -4,15 +4,19 @@ import { MenubarModule } from 'primeng/menubar';
 import { AuthService } from '../../../../business/services/auth/auth.service';
 import { take } from 'rxjs';
 import { Router } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { MenuModule } from 'primeng/menu';
 
 @Component({
   selector: 'navbar',
-  imports: [MenubarModule],
+  imports: [MenubarModule, MenuModule, AvatarGroupModule, AvatarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
   public items: MenuItem[];
+  public accountItems: MenuItem[];
 
   constructor(private authService: AuthService, private router: Router) {
     this.items = [
@@ -41,6 +45,9 @@ export class NavbarComponent {
               this.router.navigate(['library', x.userId.value])
             ),
       },
+    ];
+
+    this.accountItems = [
       {
         label: 'LogOut',
         command: () => this.authService.logout(),
