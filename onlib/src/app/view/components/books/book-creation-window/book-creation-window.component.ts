6eb16de272199efcb8364763154e47fac73f4c06
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-10-20 13:39:15 +0200

    Style windows

--- onlib/src/app/view/components/books/book-creation-window/book-creation-window.component.ts
+++ onlib/src/app/view/components/books/book-creation-window/book-creation-window.component.ts
@@ -2,6 +2,7 @@ import { CommonModule, JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import {
   FormBuilder,
+  FormControl,
   FormGroup,
   ReactiveFormsModule,
   Validators,
@@ -9,16 +10,17 @@ import {
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 import { Button } from 'primeng/button';
 import { BookCreation } from '../../../../business/models/books/bookCreation';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   standalone: true,
   selector: 'book-creation-window',
-  imports: [CommonModule, ReactiveFormsModule, JsonPipe, Button],
+  imports: [CommonModule, ReactiveFormsModule, Button, InputTextModule],
   templateUrl: './book-creation-window.component.html',
   styleUrl: './book-creation-window.component.scss',
 })
 export class BookCreationWindowComponent {
-  public form: FormGroup;
+  public form: FormGroup<{ title: FormControl<string | null> }>;
 
   constructor(private ref: DynamicDialogRef, formBuilder: FormBuilder) {
     this.form = formBuilder.group({
@@ -31,7 +33,7 @@ export class BookCreationWindowComponent {
       return;
     }
 
-    const book = new BookCreation(this.form.value.title);
+    const book = new BookCreation(this.form.value.title!);
 
     this.ref.close(book);
   }
