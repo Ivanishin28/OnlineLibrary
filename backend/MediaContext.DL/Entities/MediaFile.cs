b3875926d570eb91fa40fcf8f1bf3ff2c0a55040
Ivanishin28 <kolya.ivanishin@gmail.com>
2025-10-25 12:16:41 +0200

    Init MediaContext

---
+++ backend/MediaContext.DL/Entities/MediaFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaContext.DL.Entities
+{
+    public class MediaFile
+    {
+        public Guid Id { get; set; }
+        public byte[] Cotnext { get; set; } = null!;
+
+        public MediaFile()
+        {
+        }
+
+        public MediaFile(Guid id, byte[] cotnext)
+        {
+            Id = id;
+            Cotnext = cotnext;
+        }
+    }
+}
---
+++ backend/MediaContext.DL/MediaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using MediaContext.DL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaContext.DL
+{
+    public class MediaDbContext : DbContext
+    {
+        public DbSet<MediaFile> MediaFiles { get; set; }
+
+        public MediaDbContext(DbContextOptions<MediaDbContext> options) : base(options)
+        {
+        }
+    }
+}
