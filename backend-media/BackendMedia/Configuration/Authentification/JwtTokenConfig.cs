db7021ca5e27d86dcc29be496050d3e474639065
nivanishin <nivanishin@broadteam.eu>
2026-01-04 16:50:18 +0100

    Add Authorization to Media Backend

--- backend-media/BackendMedia/Configuration/Authentification/JwtTokenConfig.cs
+++ backend-media/BackendMedia/Configuration/Authentification/JwtTokenConfig.cs
@@ -1,4 +1,10 @@
-﻿namespace IdentityContext.Application.Configuration.Authentification
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityContext.Application.Configuration.Authentification
 {
     public record JwtTokenConfig
     {
--- backend/Identity.Application/Configuration/Authentification/Token.cs
+++ backend/Identity.Application/Configuration/Authentification/Token.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace IdentityContext.Application.Configuration.Authentification
+﻿namespace IdentityContext.Application.Configuration.Authentification
 {
     public record Token(string Value)
     {
