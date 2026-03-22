c34124692f8d8ead1f22de064436afbd4b0f2c9e
Ivanishin28 <kolya.ivanishin@gmail.com>
2026-03-22 16:24:10 +0100

    Add UploadTime to MediaFile

--- backend-media/BackendMedia/Jobs/UnusedFileCleanup/UnusedFileCleanupJob.cs
+++ backend-media/BackendMedia/Jobs/UnusedFileCleanup/UnusedFileCleanupJob.cs
@@ -16,11 +16,12 @@ public class UnusedFileCleanupJob : IJob
 
     public async Task Execute(IJobExecutionContext context)
     {
+        var cutOff = DateTime.UtcNow.AddDays(-1);
         await _db
             .MediaFiles
             .Where(x => 
                 x.Status == FileStatus.Unused ||
-                x.Status == FileStatus.Pending)
+                (x.Status == FileStatus.Pending && x.UploadTime <= cutOff))
             .ExecuteDeleteAsync(context.CancellationToken);
     }
 }
--- backend-media/MediaContext.DL/Entities/MediaFile.cs
+++ backend-media/MediaContext.DL/Entities/MediaFile.cs
@@ -8,22 +8,26 @@ public class MediaFile
     public byte[] Content { get; set; } = null!;
     public string ContentType { get; set; } = null!;
     public FileStatus Status { get; private set; }
+    public DateTime UploadTime { get; private set; }
 
     private MediaFile()
     {
     }
 
-    private MediaFile(Guid id, byte[] cotnext, string contentType, FileStatus status)
+    private MediaFile(Guid id, byte[] cotnext, string contentType, FileStatus status, DateTime uploadTime)
     {
         Id = id;
         Content = cotnext;
         ContentType = contentType;
         Status = status;
+        UploadTime = uploadTime;
     }
 
     public static MediaFile Create(Guid id, byte[] content, string contentType)
     {
-        return new MediaFile(id, content, contentType, FileStatus.Pending);
+        return new MediaFile(
+            id, content, contentType,
+            FileStatus.Pending, DateTime.UtcNow);
     }
 
     public void MarkAsInUse()
