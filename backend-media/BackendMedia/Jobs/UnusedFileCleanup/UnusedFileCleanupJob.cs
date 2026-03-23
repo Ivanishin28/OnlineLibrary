865c03ce20e0ddebd2acbcfc6c1fbb3402d3efc8
Ivanishin28 <kolya.ivanishin@gmail.com>
2026-03-23 20:54:23 +0100

    Add MediaFilesCleanupJob

---
+++ backend-media/BackendMedia/Jobs/UnusedFileCleanup/UnusedFileCleanupJob.cs
@@ -0,0 +1,26 @@
+﻿using DL;
+using DL.Enums;
+using Microsoft.EntityFrameworkCore;
+using Quartz;
+
+namespace BackendMedia.Jobs.UnusedFileCleanup;
+
+public class UnusedFileCleanupJob : IJob
+{
+    private MediaDbContext _db;
+
+    public UnusedFileCleanupJob(MediaDbContext db)
+    {
+        _db = db;
+    }
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await _db
+            .MediaFiles
+            .Where(x => 
+                x.Status == FileStatus.Unused ||
+                x.Status == FileStatus.Pending)
+            .ExecuteDeleteAsync(context.CancellationToken);
+    }
+}
---
+++ backend-media/BackendMedia/Jobs/UnusedFileCleanup/UnusedFileCleanupJobConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Options;
+using Quartz;
+
+namespace BackendMedia.Jobs.UnusedFileCleanup;
+
+public class UnusedFileCleanupJobConfiguration : IConfigureOptions<QuartzOptions>
+{
+    private static readonly JobKey Key = new(nameof(UnusedFileCleanupJob));
+    public void Configure(QuartzOptions options)
+    {
+        options
+            .AddJob<UnusedFileCleanupJob>(x => x.WithIdentity(Key))
+            .AddTrigger(o =>
+                o.ForJob(Key)
+                    .WithSimpleSchedule(s =>
+                        s.WithIntervalInHours(24)
+                            .RepeatForever()
+                    )
+                    .StartAt(GetNextRunTime())
+            );
+    }
+
+    private DateTime GetNextRunTime()
+    {
+        var now = DateTime.UtcNow;
+        var nextRun = new DateTime(now.Year, now.Month, now.Day, 0, 0, 0, DateTimeKind.Utc);
+        return now >= nextRun 
+            ? nextRun.AddDays(1) 
+            : nextRun;
+    }
+}
